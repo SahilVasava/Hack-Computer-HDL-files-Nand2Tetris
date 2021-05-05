c0378fd7b5d1d03d732695c3136c0dd90e4f37e8
Sahil Vasava <sahilvasava@gmail.com>
2021-05-05 20:09:51 +0530

    project assignment files upto project 10

---
+++ 04/fill/Fill.asm
@@ -0,0 +1,99 @@
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/04/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input.
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel;
+// the screen should remain fully black as long as the key is pressed. 
+// When no key is pressed, the program clears the screen, i.e. writes
+// "white" in every pixel;
+// the screen should remain fully clear as long as no key is pressed.
+
+// Put your code here.
+// key = KBD
+// val = 0
+//Loop:
+// if key == 0 goto white
+// if key != 0 goto black
+//black:
+//val = -1
+//goto fill
+//white:
+//val = 0
+//goto fill
+//Fill:
+// addr = SCREEN
+// i = 0
+// n = Ram[0] 8191
+// Floop:  
+//   if i > n goto Loop
+//   RAM[addr] = val
+//   addr = addr + 1
+//   i = i + 1
+// goto floop
+// 
+
+(LOOP)
+    @0
+    D=A
+    @val
+    M=D
+    @KBD
+    D=M
+    @key
+    M=D
+    @WHITE
+    D;JEQ
+    @BLACK
+    0;JMP
+
+(BLACK)
+    @val
+    M=-1 
+    @FILL
+    0;JMP
+
+(WHITE)
+    @val
+    M=0 
+    @FILL
+    0;JMP
+
+(FILL)
+    @SCREEN 
+    D=A
+    @addr
+    M=D 
+    @0
+    D=A
+    @i
+    M=D
+    @R0
+    D=M
+    @n
+    M=D 
+
+(FLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @LOOP
+    D;JGT
+    @val
+    D=M
+    @addr
+    A=M
+    M=D
+    @4
+    D=A
+    @addr
+    M=D+M
+    @4
+    D=A
+    @i
+    M=M+D
+    @FLOOP
+    0;JMP
